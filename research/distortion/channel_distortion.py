b7a6b3b7c9cdb4b2b28f670d1e9b9090fdacc815
Yakir Gorski <yakirg320@gmail.com>
2022-11-02 12:52:26 +0200

    Full pipeline

--- research/distortion/channel_distortion.py
+++ research/distortion/channel_distortion.py
@@ -1,24 +1,13 @@
 import argparse
 import os
 from tqdm import tqdm
-import torch
 import numpy as np
 from typing import Dict, List
-import glob
 import pickle
-import shutil
 
-from research.block_relu.consts import TARGET_REDUCTIONS
-from research.block_relu.utils import get_model, get_data, center_crop, ArchUtilsFactory
-from research.block_relu.params import ParamsFactory, MobileNetV2_256_Params
 from research.parameters.base import MobileNetV2_256_Params_2_Groups
 from research.distortion.distortion_utils import DistortionUtils
-from research.pipeline.backbones.secure_resnet import MyResNet  # TODO: find better way to init
 
-from mmseg.ops import resize
-import torch.nn.functional as F
-from mmseg.core import intersect_and_union
-import pandas as pd
 
 class ChannelDistortionHandler:
     def __init__(self, gpu_id, output_path, params):
@@ -44,7 +33,8 @@ class ChannelDistortionHandler:
             input_block_name = self.params.LAYER_NAME_TO_BLOCK_NAME[layer_name]
             output_block_name = self.params.IN_LAYER_PROXY_SPEC[layer_name]
 
-            layer_assets = {key: np.zeros(shape=(layer_num_channels, len(block_sizes), batch_size)) for key in self.keys}
+            layer_assets = {key: np.zeros(shape=(layer_num_channels, len(block_sizes), batch_size)) for key in
+                            self.keys}
             for channel in tqdm(range(layer_num_channels), desc=f"Batch={batch_index} Layer={layer_name}"):
                 for block_size_index, block_size in enumerate(block_sizes):
                     layer_block_sizes = np.ones(shape=(layer_num_channels, 2), dtype=np.int32)  # TODO: reuse
@@ -66,26 +56,40 @@ class ChannelDistortionHandler:
             with open(file_name, "wb") as file:
                 pickle.dump(obj=layer_assets, file=file)
 
+
 if __name__ == "__main__":
 
-    gpu_id = 1
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--batch_index', type=int)
+    parser.add_argument('--gpu_id', type=int)
+    parser.add_argument('--dataset', type=str)
+    parser.add_argument('--config', type=str)
+    parser.add_argument('--checkpoint', type=str)
+    parser.add_argument('--iter', type=int)
+    parser.add_argument('--block_size_spec_file_name', type=str, default=None)
+    parser.add_argument('--output_path', type=str, default=None)
+    args = parser.parse_args()
+
+    gpu_id = args.gpu_id
     params = MobileNetV2_256_Params_2_Groups()
-    params.DATASET = "ade_20k"
-    params.CONFIG = "/home/yakir/PycharmProjects/secure_inference/research/pipeline/configs/deeplabv3_m-v2-d8_256x256_160k_ade20k.py"
-    params.CHECKPOINT = "/home/yakir/PycharmProjects/secure_inference/work_dirs/deeplabv3_m-v2-d8_256x256_160k_ade20k/iter_160000.pth"
+    params.DATASET = args.dataset
+    params.CONFIG = args.config
+    params.CHECKPOINT = args.checkpoint
 
-    block_size_spec_f_format = "/home/yakir/Data2/block_relu_specs/deeplabv3_m-v2-d8_256x256_160k_ade20k_2_groups_iter_{}_0.0833.pickle"
-    output_path = "/home/yakir/Data2/assets_v4/distortions/ade_20k/MobileNetV2_256/channels_distortion_2_groups"
+    output_path = args.output_path
+    os.makedirs(output_path, exist_ok=True)
 
-    iter_ = 1
-    layer_names = params.LAYER_GROUPS[iter_]
-    if iter_ == 0:
-        baseline_block_size_spec = dict()
+    iteration = args.iter
+    layer_names = params.LAYER_GROUPS[iteration]
+
+    if args.block_size_spec_file_name and os.path.exists(args.block_size_spec_file_name):
+        baseline_block_size_spec = pickle.load(open(args.block_size_spec_file_name, 'rb'))
     else:
-        baseline_block_size_spec = pickle.load(open(block_size_spec_f_format.format(iter_-1), 'rb'))
+        baseline_block_size_spec = dict()
 
     if "decode_0" in layer_names:
         layer_names.remove("decode_0")
+
     chd = ChannelDistortionHandler(gpu_id=gpu_id,
                                    output_path=output_path,
                                    params=params)
