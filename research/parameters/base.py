42e552a81e2795e86dbbaa5a1dc1353353a73038
Yakir Gorski <yakirg320@gmail.com>
2022-11-07 07:55:27 +0200

    rearranging params

--- research/parameters/base.py
+++ research/parameters/base.py
@@ -298,8 +298,7 @@ BLOCK_SIZES_MINI = [
 
 BLOCK_SIZE_COMPLETE = [[i, j] for i in range(1, 65) for j in range(1, 65)]
 
-
-class MobileNetV2_256_Params:
+class MobileNetV2_Params:
     def __init__(self):
         self.BACKBONE = "MobileNetV2"
 
@@ -415,6 +414,30 @@ class MobileNetV2_256_Params:
                 "decode_5",
             ]
 
+        self.BLOCK_INPUT_DICT = \
+            {
+                "conv1": "input_images",
+                "layer1_0": "conv1",
+                "layer2_0": "layer1_0",
+                "layer2_1": "layer2_0",
+                "layer3_0": "layer2_1",
+                "layer3_1": "layer3_0",
+                "layer3_2": "layer3_1",
+                "layer4_0": "layer3_2",
+                "layer4_1": "layer4_0",
+                "layer4_2": "layer4_1",
+                "layer4_3": "layer4_2",
+                "layer5_0": "layer4_3",
+                "layer5_1": "layer5_0",
+                "layer5_2": "layer5_1",
+                "layer6_0": "layer5_2",
+                "layer6_1": "layer6_0",
+                "layer6_2": "layer6_1",
+                "layer7_0": "layer6_2",
+                "decode": "layer7_0",
+                None: "decode"
+            }
+
         self.LAYER_NAME_TO_BLOCK_SIZES = \
             {
                 'conv1': BLOCK_SIZES_FULL,
@@ -459,32 +482,17 @@ class MobileNetV2_256_Params:
                 'decode_5': BLOCK_SIZES_MINI
             }
 
-        self.BLOCK_INPUT_DICT = \
-            {
-                "conv1": "input_images",
-                "layer1_0": "conv1",
-                "layer2_0": "layer1_0",
-                "layer2_1": "layer2_0",
-                "layer3_0": "layer2_1",
-                "layer3_1": "layer3_0",
-                "layer3_2": "layer3_1",
-                "layer4_0": "layer3_2",
-                "layer4_1": "layer4_0",
-                "layer4_2": "layer4_1",
-                "layer4_3": "layer4_2",
-                "layer5_0": "layer4_3",
-                "layer5_1": "layer5_0",
-                "layer5_2": "layer5_1",
-                "layer6_0": "layer5_2",
-                "layer6_1": "layer6_0",
-                "layer6_2": "layer6_1",
-                "layer7_0": "layer6_2",
-                "decode": "layer7_0",
-                None: "decode"
-            }
 
-        self.LAYER_NAME_TO_DIMS = {'conv1': [32, 256, 256], 'layer1_0_0': [32, 128, 128], 'layer2_0_0': [96, 128, 128],
-                                   'layer2_0_1': [96, 64, 64], 'layer2_1_0': [144, 64, 64], 'layer2_1_1': [144, 64, 64],
+class MobileNetV2_256_Params(MobileNetV2_Params):
+    def __init__(self):
+        super().__init__()
+
+        self.LAYER_NAME_TO_DIMS = {'conv1': [32, 128, 128],
+                                   'layer1_0_0': [32, 128, 128],
+                                   'layer2_0_0': [96, 128, 128],
+                                   'layer2_0_1': [96, 64, 64],
+                                   'layer2_1_0': [144, 64, 64],
+                                   'layer2_1_1': [144, 64, 64],
                                    'layer3_0_0': [144, 64, 64],
                                    'layer3_0_1': [144, 32, 32],
                                    'layer3_1_0': [192, 32, 32],
@@ -513,10 +521,57 @@ class MobileNetV2_256_Params:
                                    'layer6_2_1': [960, 32, 32],
                                    'layer7_0_0': [960, 32, 32],
                                    'layer7_0_1': [960, 32, 32],
-                                   'decode_0': [512, 1, 1],
-                                   'decode_1': [512, 32, 32], 'decode_2': [512, 32, 32], 'decode_3': [512, 32, 32],
-                                   'decode_4': [512, 32, 32], 'decode_5': [512, 32, 32]}
+                                   'decode_0':   [512, 1, 1],
+                                   'decode_1':   [512, 32, 32],
+                                   'decode_2':   [512, 32, 32],
+                                   'decode_3':   [512, 32, 32],
+                                   'decode_4':   [512, 32, 32],
+                                   'decode_5':   [512, 32, 32]}
 
+class MobileNetV2_512_Params(MobileNetV2_Params):
+    def __init__(self):
+        super().__init__()
+
+        self.LAYER_NAME_TO_DIMS = {'conv1': [32, 256, 256],
+                                   'layer1_0_0': [32, 256, 256],
+                                   'layer2_0_0': [96, 256, 256],
+                                   'layer2_0_1': [96, 128, 128],
+                                   'layer2_1_0': [144, 128, 128],
+                                   'layer2_1_1': [144, 128, 128],
+                                   'layer3_0_0': [144, 128, 128],
+                                   'layer3_0_1': [144, 64, 64],
+                                   'layer3_1_0': [192, 64, 64],
+                                   'layer3_1_1': [192, 64, 64],
+                                   'layer3_2_0': [192, 64, 64],
+                                   'layer3_2_1': [192, 64, 64],
+                                   'layer4_0_0': [192, 64, 64],
+                                   'layer4_0_1': [192, 64, 64],
+                                   'layer4_1_0': [384, 64, 64],
+                                   'layer4_1_1': [384, 64, 64],
+                                   'layer4_2_0': [384, 64, 64],
+                                   'layer4_2_1': [384, 64, 64],
+                                   'layer4_3_0': [384, 64, 64],
+                                   'layer4_3_1': [384, 64, 64],
+                                   'layer5_0_0': [384, 64, 64],
+                                   'layer5_0_1': [384, 64, 64],
+                                   'layer5_1_0': [576, 64, 64],
+                                   'layer5_1_1': [576, 64, 64],
+                                   'layer5_2_0': [576, 64, 64],
+                                   'layer5_2_1': [576, 64, 64],
+                                   'layer6_0_0': [576, 64, 64],
+                                   'layer6_0_1': [576, 64, 64],
+                                   'layer6_1_0': [960, 64, 64],
+                                   'layer6_1_1': [960, 64, 64],
+                                   'layer6_2_0': [960, 64, 64],
+                                   'layer6_2_1': [960, 64, 64],
+                                   'layer7_0_0': [960, 64, 64],
+                                   'layer7_0_1': [960, 64, 64],
+                                   'decode_0': [512, 1, 1],
+                                   'decode_1': [512, 64, 64],
+                                   'decode_2': [512, 64, 64],
+                                   'decode_3': [512, 64, 64],
+                                   'decode_4': [512, 64, 64],
+                                   'decode_5': [512, 64, 64]}
 
 class MobileNetV2_256_Params_2_Groups(MobileNetV2_256_Params):
     def __init__(self):
@@ -613,3 +668,108 @@ class MobileNetV2_256_Params_2_Groups(MobileNetV2_256_Params):
                 'decode_5': "decode"
             }
 
+class MobileNetV2_512_Params_2_Groups(MobileNetV2_512_Params):
+    def __init__(self):
+
+        super().__init__()
+        self.LAYER_GROUPS = \
+            [
+                [
+                    "conv1",
+                    "layer1_0_0",
+                    "layer2_0_0",
+                    "layer2_0_1",
+                    "layer2_1_0",
+                    "layer2_1_1",
+                    "layer3_0_0",
+                    "layer3_0_1",
+                    "layer3_1_0",
+                    "layer3_1_1",
+                    "layer3_2_0",
+                    "layer3_2_1",
+                    "layer4_0_0",
+                    "layer4_0_1",
+                    "layer4_1_0",
+                    "layer4_1_1",
+                    "layer4_2_0",
+                    "layer4_2_1",
+                    "layer4_3_0",
+                    "layer4_3_1",
+                    "layer5_0_0",
+                    "layer5_0_1",
+                    "layer5_1_0",
+                    "layer5_1_1",
+                    "layer5_2_0",
+                    "layer5_2_1",
+                    "layer6_0_0",
+                    "layer6_0_1",
+                    "layer6_1_0",
+                    "layer6_1_1",
+                    "layer6_2_0",
+                    "layer6_2_1",
+                    "layer7_0_0",
+                    "layer7_0_1",
+                ],
+                [
+                    "decode_0",
+                    "decode_1",
+                    "decode_2",
+                    "decode_3",
+                    "decode_4",
+                    "decode_5",
+                ]
+            ]
+        self.IN_LAYER_PROXY_SPEC = \
+            {
+                'conv1':      'layer7_0',
+                'layer1_0_0': 'layer7_0',
+                'layer2_0_0': 'layer7_0',
+                'layer2_0_1': 'layer7_0',
+                'layer2_1_0': 'layer7_0',
+                'layer2_1_1': 'layer7_0',
+                'layer3_0_0': 'layer7_0',
+                'layer3_0_1': 'layer7_0',
+                'layer3_1_0': 'layer7_0',
+                'layer3_1_1': 'layer7_0',
+                'layer3_2_0': 'layer7_0',
+                'layer3_2_1': 'layer7_0',
+                'layer4_0_0': 'layer7_0',
+                'layer4_0_1': 'layer7_0',
+                'layer4_1_0': 'layer7_0',
+                'layer4_1_1': 'layer7_0',
+                'layer4_2_0': 'layer7_0',
+                'layer4_2_1': 'layer7_0',
+                'layer4_3_0': 'layer7_0',
+                'layer4_3_1': 'layer7_0',
+                'layer5_0_0': 'layer7_0',
+                'layer5_0_1': 'layer7_0',
+                'layer5_1_0': 'layer7_0',
+                'layer5_1_1': 'layer7_0',
+                'layer5_2_0': 'layer7_0',
+                'layer5_2_1': 'layer7_0',
+                'layer6_0_0': 'layer7_0',
+                'layer6_0_1': 'layer7_0',
+                'layer6_1_0': 'layer7_0',
+                'layer6_1_1': 'layer7_0',
+                'layer6_2_0': 'layer7_0',
+                'layer6_2_1': 'layer7_0',
+                'layer7_0_0': 'layer7_0',
+                'layer7_0_1': 'layer7_0',
+                'decode_0': "decode",
+                'decode_1': "decode",
+                'decode_2': "decode",
+                'decode_3': "decode",
+                'decode_4': "decode",
+                'decode_5': "decode"
+            }
+
+
+class ParamsFactory:
+    def __init__(self):
+        self.classes = {
+            "MobileNetV2_256_Params_2_Groups": MobileNetV2_256_Params_2_Groups,
+            "MobileNetV2_512_Params_2_Groups": MobileNetV2_512_Params_2_Groups,
+        }
+
+    def __call__(self, type_):
+        return self.classes[type_]()
