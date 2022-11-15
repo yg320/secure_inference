ed3ea5653c736ae7775b97ecd8e4485f2e01f5c3
Yakir Gorski <yakirg320@gmail.com>
2022-11-15 19:27:18 +0200

    job fetcher organization

--- research/job_fetcher.py
+++ research/job_fetcher.py
@@ -9,53 +9,56 @@ class JobFetcher:
 
     def job(self):
 
-        checkpoint = "/home/yakir/PycharmProjects/secure_inference/work_dirs/m-v2_256x256_ade20k/baseline/iter_160000.pth"
+        ratio = 0.1
+        iter_to_checkpoint = ["/home/yakir/PycharmProjects/secure_inference/work_dirs/m-v2_256x256_ade20k/baseline/iter_160000.pth",
+                              "/home/yakir/PycharmProjects/secure_inference/work_dirs/m-v2_256x256_ade20k/baseline/iter_160000.pth"]
         config = "/home/yakir/PycharmProjects/secure_inference/work_dirs/m-v2_256x256_ade20k/baseline/baseline.py"
         dataset = "ade_20k_256x256"
-        # ratio = 0.08333333333333333
-        ratio = 0.0625
-        base_output_path = f"/home/yakir/Data2/assets_v4/distortions/{dataset}/MobileNetV2/2_groups_160k_{ratio}"
+        base_output_path = f"/home/yakir/Data2/assets_v4/distortions/ade_20k_256x256/MobileNetV2/benchmark"
         output_path = os.path.join(base_output_path, "channel_distortions")
         block_size_spec_file_name = os.path.join(base_output_path, "block_spec.pickle")
         script_path = "/home/yakir/PycharmProjects/secure_inference/research/distortion/channel_distortion.py"
         knapsack_script_path = "/home/yakir/PycharmProjects/secure_inference/research/knapsack/multiple_choice_knapsack.py"
         params_name = "MobileNetV2_256_Params_2_Groups"
 
-        for iteration in range(2):
-            jobs = []
-            for gpu_id in range(2):
+        for iteration in range(0, 2):
 
-                job_params = [
-                    "--batch_index", f"{gpu_id}",
-                    "--gpu_id", f"{gpu_id}",
-                    "--dataset", dataset,
-                    "--config", config,
-                    "--checkpoint", checkpoint,
-                    "--iter", f"{iteration}",
-                    "--block_size_spec_file_name", block_size_spec_file_name,
-                    "--output_path", output_path,
-                    "--params_name", params_name
-                ]
-                jobs.append(["python", script_path] + job_params)
+            for batch_index in [0, 1, 2]:
+                jobs = []
+                for gpu_id in [0, 1]:
+                    job_params = [
+                        "--batch_index", f"{2 * batch_index + gpu_id}",
+                        "--gpu_id", f"{gpu_id}",
+                        "--dataset", dataset,
+                        "--config", config,
+                        "--checkpoint", iter_to_checkpoint[iteration],
+                        "--iter", f"{iteration}",
+                        "--block_size_spec_file_name", block_size_spec_file_name,
+                        "--output_path", output_path,
+                        "--params_name", params_name
+                    ]
+                    jobs.append(["python", script_path] + job_params)
 
-            # running_jobs = [subprocess.Popen(job) for job in jobs]
-            #
-            # for running_job in running_jobs:
-            #     running_job.wait()
+                running_jobs = [subprocess.Popen(job) for job in jobs]
+
+                for running_job in running_jobs:
+                    running_job.wait()
 
             knapsack_job_params = [
-                "--dataset", dataset,
-                "--config", config,
-                "--checkpoint", checkpoint,
+
                 "--iter", f"{iteration}",
                 "--block_size_spec_file_name", block_size_spec_file_name,
                 "--output_path", output_path,
                 '--ratio', f"{ratio}",
-                "--params_name", params_name
+                "--params_name", params_name,
+                "--cost_type", "Bandwidth",
+                "--division", "512"
+
             ]
             job = ["python", knapsack_script_path] + knapsack_job_params
-            # running_job = subprocess.Popen(job)
-            # running_job.wait()
+            running_job = subprocess.Popen(job)
+            running_job.wait()
+
 
 if __name__ == "__main__":
     JobFetcher().job()
