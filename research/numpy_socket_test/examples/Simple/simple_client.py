40969059e9ba115189f7d444a36cc989ffcf2772
Yakir Gorski <yakirg320@gmail.com>
2022-12-04 18:32:26 +0200

    Code that allows for numpy over sockets

---
+++ research/numpy_socket_test/examples/Simple/simple_client.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import logging
+import numpy as np
+from research.numpy_socket_test.numpysocket.numpysocket import NumpySocket
+import socket
+
+logger = logging.getLogger('simple client')
+logger.setLevel(logging.INFO)
+
+with NumpySocket() as s:
+    s.connect(("localhost", 9999))
+    
+    logger.info("sending numpy array:")
+    frame = np.arange(1000)
+    s.sendall(frame)
+
