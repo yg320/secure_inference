4e3a3ab629d76d9e66a9fb87bbe55ffc7b3f72aa
Yakir Gorski <yakirg320@gmail.com>
2022-12-04 18:31:03 +0200

    Job fetcher related code

--- research/job_fetcher_2.py
+++ research/job_fetcher_2.py
@@ -10,19 +10,18 @@ class JobFetcher:
     def job(self):
 
         ratio = 0.1
-        iter_to_checkpoint = ["/home/yakir/PycharmProjects/secure_inference/work_dirs/m-v2_256x256_ade20k/baseline/iter_160000.pth",
-                              "/home/yakir/PycharmProjects/secure_inference/work_dirs/m-v2_256x256_ade20k/baseline/iter_160000.pth"]
+        iter_to_checkpoint = ["/home/yakir/PycharmProjects/secure_inference/work_dirs/m-v2_256x256_ade20k/snr/iter_20000.pth",
+                              "/home/yakir/PycharmProjects/secure_inference/work_dirs/m-v2_256x256_ade20k/snr/iter_20000.pth"]
         config = "/home/yakir/PycharmProjects/secure_inference/work_dirs/m-v2_256x256_ade20k/baseline/baseline.py"
         dataset = "ade_20k_256x256"
-        base_output_path = f"/home/yakir/Data2/assets_v4/distortions/ade_20k_256x256/MobileNetV2/snr"
+        base_output_path = f"/home/yakir/Data2/assets_v4/distortions/ade_20k_256x256/MobileNetV2/snr_2"
         output_path = os.path.join(base_output_path, "channel_distortions")
-        # block_size_spec_file_name = "" #os.path.join(base_output_path, "block_spec.pickle")
+        block_size_spec_file_name = "/home/yakir/Data2/assets_v4/distortions/ade_20k_256x256/MobileNetV2/snr_2/block_size_spec.pickle"
         script_path = "/home/yakir/PycharmProjects/secure_inference/research/distortion/channel_distortion.py"
         knapsack_script_path = "/home/yakir/PycharmProjects/secure_inference/research/knapsack/multiple_choice_knapsack.py"
         params_name = "MobileNetV2_256_Params_2_Groups"
 
-        for iteration in range(1, 2):
-
+        for iteration in range(0, 1):
             for batch_index in [0, 1, 2]:
                 jobs = []
                 for gpu_id in [0, 1]:
@@ -33,7 +32,7 @@ class JobFetcher:
                         "--config", config,
                         "--checkpoint", iter_to_checkpoint[iteration],
                         "--iter", f"{iteration}",
-                        # "--block_size_spec_file_name", block_size_spec_file_name,
+                        "--block_size_spec_file_name", block_size_spec_file_name,
                         "--output_path", output_path,
                         "--params_name", params_name
                     ]
