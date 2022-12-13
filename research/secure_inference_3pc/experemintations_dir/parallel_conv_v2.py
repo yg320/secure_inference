2c01815baceea3da527ddc6b198aca2fa9bd267f
Yakir Gorski <yakirg320@gmail.com>
2022-12-13 15:32:41 +0200

    Trying to boost conv2d

--- research/secure_inference_3pc/experemintations_dir/parallel_conv_v2.py
+++ research/secure_inference_3pc/experemintations_dir/parallel_conv_v2.py
@@ -1,39 +1,32 @@
 import numpy as np
-import torch
 
 import time
 from numba import njit, prange
 
 @njit(parallel=True)
 def conv(A, B):
-    k0, k1 = B.shape[2:]
 
-    assert k0 % 2 == 1
-    assert k1 % 2 == 1
+    res = np.zeros((B.shape[0], A.shape[2], A.shape[3]), dtype=np.int64)
 
+    for out_channel in prange(B.shape[0]):
+        for in_channel in range(A.shape[1]):
+            for i in range(A.shape[2]):
+                for j in range(A.shape[3]):
+                    for k in range(3):
+                        for l in range(3):
+                            res[out_channel, i, j] += B[out_channel, in_channel, k, l] * A[0,in_channel, i, j]
 
-    res = np.zeros((B.shape[0], A.shape[2], A.shape[3]), dtype=A.dtype)
-
-
-    for m in range(1, A.shape[2] - 1):
-        for l in range(1, A.shape[3] - 1):
-            a = A[:, :, m-1:m+2, l-1:l+2]
-            b = B
-            tt = (a*b).sum(axis=(1,2,3))
-            res[:, m, l] += tt
     return res
 
-A = np.random.randint(np.iinfo(np.int64).min, np.iinfo(np.int64).max, size=(1, 512, 64, 64))
-B = np.random.randint(np.iinfo(np.int64).min, np.iinfo(np.int64).max, size=(512, 512, 3, 3))
 
-# a = A[:, :3, :3][np.newaxis]
-# b = B
-# (a*b).sum(axis=(1,2,3))
-# C = np.random.randint(np.iinfo(np.int64).min, np.iinfo(np.int64).max, size=(512, 64, 64))
-# D = np.random.randint(np.iinfo(np.int64).min, np.iinfo(np.int64).max, size=(512, 512, 3, 3))
-t0 = time.time()
-res = conv(A, B)
-print(time.time() - t0)
+# ================== (1, 640, 24, 24) (128, 640, 3, 3)
+# ================== (1, 576, 5760) (5760, 128)
 
-# torch.Size([1, 4096, 4608])
-# torch.Size([4608, 512])
+
+for i in range(10):
+    A = np.random.randint(np.iinfo(np.int64).min, np.iinfo(np.int64).max, size=(1, 640, 24, 24))
+    B = np.random.randint(np.iinfo(np.int64).min, np.iinfo(np.int64).max, size=(128, 640, 3, 3))
+    print(A.shape[2], A.shape[3])
+    t0 = time.time()
+    res = conv(A, B)
+    print(time.time() - t0)
