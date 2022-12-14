941bf2a4a54ed145947d72caae7708ad72f6441b
Yakir Gorski <yakirg320@gmail.com>
2022-12-14 13:33:41 +0200

    Added support for replace ReLUs with Seucre Block ReLUs

--- research/distortion/utils.py
+++ research/distortion/utils.py
@@ -212,8 +212,8 @@ class ArchUtils:
         for layer_name, layer in layer_names_to_layers.items():
             self.set_layer(model, layer_name, layer)
 
-    def set_bReLU_layers(self, model, layer_name_to_block_sizes, num_soft_start_steps=0):
-        layer_name_to_layers = {layer_name: BlockRelu(block_sizes=block_sizes, num_soft_start_steps=num_soft_start_steps)
+    def set_bReLU_layers(self, model, layer_name_to_block_sizes, block_relu_class):
+        layer_name_to_layers = {layer_name: block_relu_class(block_sizes=block_sizes)
                                 for layer_name, block_sizes in layer_name_to_block_sizes.items()}
         self.set_layers(model, layer_name_to_layers)
 
