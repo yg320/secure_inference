246472f1cc3d37a2f2d0e0458253f0ea23a5e90c
Yakir Gorski <yakirg320@gmail.com>
2022-12-15 13:48:12 +0200

    Setting up Docker

---
+++ docker/docker_entry_point.py
@@ -0,0 +1,3 @@
+import os
+os.chdir("/storage/yakir/secure_inference")
+os.system("python exe_script.py")
---
+++ exe_script.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("bash ./research/pipeline/dist_train.sh research/pipeline/configs/m-v2_256x256_ade20k/baseline_test.py 2")
