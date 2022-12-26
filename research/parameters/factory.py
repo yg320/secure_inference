0984d9f2dede7dda9c331dd7234c485c5ee3a2b1
Yakir Gorski <yakirg320@gmail.com>
2022-12-26 15:06:17 +0200

    (1) Changed logic of paramets factory. (2) Added parameters of classification ResNet

--- research/parameters/factory.py
+++ research/parameters/factory.py
@@ -1,17 +1,20 @@
 from research.parameters.mobile_net import MobileNetV2_256_Params_2_Groups, MobileNetV2_512_Params_2_Groups, MobileNetV2_256_Params_1_Groups, MobileNetV2_256_Params_1_Groups_mini
 from research.parameters.resnet_18 import ResNet18_Params_96x96, ResNet18_Params_192x192
 
+from research.parameters.classification.ResNet_CIFAR import ResNet18_CIFAR, ResNet50_CIFAR
 
 class ParamsFactory:
     def __init__(self):
-        self.classes = {
-            "MobileNetV2_256_Params_2_Groups": MobileNetV2_256_Params_2_Groups,
-            "MobileNetV2_512_Params_2_Groups": MobileNetV2_512_Params_2_Groups,
-            "MobileNetV2_256_Params_1_Groups": MobileNetV2_256_Params_1_Groups,
-            "MobileNetV2_256_Params_1_Groups_mini": MobileNetV2_256_Params_1_Groups_mini,
-            "ResNet18_Params_96x96": ResNet18_Params_96x96,
-            "ResNet18_Params_192x192": ResNet18_Params_192x192,
-        }
+        pass
 
-    def __call__(self, type_):
-        return self.classes[type_]()
+    def __call__(self, model_cfg):
+        if model_cfg.type == 'ResNet_CIFAR_V2':
+            if model_cfg.depth == 18:
+                return ResNet18_CIFAR()
+            if model_cfg.depth == 50:
+                return ResNet50_CIFAR()
+        else:
+            assert False, "Unknown model type: {}".format(model_cfg.type)
+        # return self.classes[type_]()
+
+param_factory = ParamsFactory()
