93ad383ed7c5bdeec1f23a0e65c7fc53615cc5c5
Yakir Gorski <yakirg320@gmail.com>
2022-12-27 05:22:49 +0200

    (1) Added support for 64x64, 128x128, 256x256 block sizes. (2) Added MobileNet to factory

--- research/distortion/parameters/factory.py
+++ research/distortion/parameters/factory.py
@@ -1,6 +1,7 @@
 from research.distortion.parameters.mobile_net import MobileNetV2_256_Params_2_Groups, MobileNetV2_512_Params_2_Groups, MobileNetV2_256_Params_1_Groups, MobileNetV2_256_Params_1_Groups_mini
 from research.distortion.parameters.resnet_18 import ResNet18_Params_96x96, ResNet18_Params_192x192
 from research.distortion.parameters.classification.ResNet_CIFAR import ResNet18_CIFAR, ResNet50_CIFAR
+from research.distortion.parameters.segmentation.MobileNetV2 import MobileNetV2
 
 class ParamsFactory:
     def __init__(self):
@@ -12,8 +13,9 @@ class ParamsFactory:
                 return ResNet18_CIFAR()
             if model_cfg.depth == 50:
                 return ResNet50_CIFAR()
+        if model_cfg.type == 'MobileNetV2':
+            return MobileNetV2()
         else:
             assert False, "Unknown model type: {}".format(model_cfg.type)
-        # return self.classes[type_]()
 
 param_factory = ParamsFactory()
