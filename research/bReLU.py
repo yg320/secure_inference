b7ce98d2b91cf40ceb146f5484f7152eef84b633
Yakir Gorski <yakirg320@gmail.com>
2022-12-29 14:03:39 +0200

    Implemented a bit-exact secure-bReLU

--- research/bReLU.py
+++ research/bReLU.py
@@ -45,147 +45,213 @@ class BlockRelu(Module):
 
         return relu_map.mul_(activation)
 
+from abc import ABCMeta, abstractmethod
 
-class DepthToSpace(Module):
+
+class DepthToSpace(Module, metaclass=ABCMeta):
 
     def __init__(self, block_size):
         super().__init__()
         self.block_size = block_size
+
+    @abstractmethod
+    def permute(self, x, dims):
+        pass
 
     def forward(self, x):
         N, C, H, W, _ = x.shape
         x = x.reshape(N, C, H, W, self.block_size[0], self.block_size[1])
-        x = x.permute(0, 1, 2, 4, 3, 5)
+        x = self.permute(x, (0, 1, 2, 4, 3, 5))
         x = x.reshape(N, C, H * self.block_size[0], W * self.block_size[1])
         return x
 
-
-class SpaceToDepth(Module):
+class SpaceToDepth(Module, metaclass=ABCMeta):
 
     def __init__(self, block_size):
         super().__init__()
         self.block_size = block_size
 
+    @abstractmethod
+    def permute(self, x, dims):
+        pass
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+
         N, C, H, W = x.shape
         x = x.reshape(N, C, H // self.block_size[0], self.block_size[0], W // self.block_size[1], self.block_size[1])
-
-        x = x.permute(0, 1, 2, 4, 3, 5)
+        x = self.permute(x, (0, 1, 2, 4, 3, 5))  #Numpy
         x = x.reshape(N, C, H // self.block_size[0], W // self.block_size[1], self.block_size[0] * self.block_size[1])
         return x
 
 
-class Pad:
+class DepthToSpaceNumpy(DepthToSpace):
+    def __init__(self, block_size):
+        super().__init__(block_size=block_size)
+
+    def permute(self, x, order):
+        return x.transpose(order)
+
+
+class SpaceToDepthNumpy(SpaceToDepth):
+    def __init__(self, block_size):
+        super().__init__(block_size=block_size)
+
+    def permute(self, x, order):
+        return x.transpose(order)
+
+
+class DepthToSpaceTorch(DepthToSpace):
+    def __init__(self, block_size):
+        super().__init__(block_size=block_size)
+
+    def permute(self, x, order):
+        return x.permute(order)
+
+
+class SpaceToDepthTorch(SpaceToDepth):
+    def __init__(self, block_size):
+        super().__init__(block_size=block_size)
+
+    def permute(self, x, order):
+        return x.permute(order)
+
+
+class PadHandler(metaclass=ABCMeta):
 
     def __init__(self, x, block_size):
         super().__init__()
-        if x.shape[2] % block_size[0] != 0 or x.shape[3] % block_size[1] != 0:
-            self.should_pad = True
-            self.pad_x_l = 0
-            self.pad_x_r = block_size[0] - x.shape[2] % block_size[0]
-            self.pad_y_l = 0
-            self.pad_y_r = block_size[1] - x.shape[3] % block_size[1]
-            self.pad = torch.nn.ZeroPad2d((self.pad_x_l, self.pad_x_r, self.pad_y_l, self.pad_y_r))
-        else:
-            self.should_pad = False
-            self.pad = torch.nn.Identity()
-    #
-    # def pad(self, x):
-    #     if self.should_pad:
-    #         pad_values = [self.pad_y_l, self.pad_y_r, self.pad_x_l, self.pad_x_r]
-    #         return F.pad(x, pad=pad_values, mode='constant', value=0)
-    #     else:
-    #         return x
+        self.pad_x_l = 0
+        self.pad_x_r = (block_size[0] - x.shape[2] % block_size[0]) % block_size[0]
+        self.pad_y_l = 0
+        self.pad_y_r = (block_size[1] - x.shape[3] % block_size[1]) % block_size[1]
+
+    @abstractmethod
+    def pad(self, x):
+        pass
 
     def unpad(self, x):
-        if self.should_pad:
-            return x[:, :, self.pad_x_l:x.shape[2] - self.pad_x_r, self.pad_y_l:x.shape[3] - self.pad_y_r]
-        else:
-            return x
+        return x[:, :, self.pad_x_l:x.shape[2] - self.pad_x_r, self.pad_y_l:x.shape[3] - self.pad_y_r]
 
-class BlockReLU_V1(Module):
+
+class TorchPadHandler(PadHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def pad(self, x):
+        return F.pad(x, (self.pad_y_l, self.pad_y_r, self.pad_x_l, self.pad_x_r), 'constant', 0)
+
+
+class NumpyPadHandler(PadHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def pad(self, x):
+        return np.pad(x, ((0, 0), (0, 0), (self.pad_x_l, self.pad_x_r), (self.pad_y_l, self.pad_y_r)), 'constant')
+
+
+class SecureOptimizedBlockReLU(Module):
 
     def __init__(self, block_sizes):
-        super(BlockReLU_V1, self).__init__()
+        super(SecureOptimizedBlockReLU, self).__init__()
         self.block_sizes = np.array(block_sizes)
 
-        self.active_block_sizes = [block_size for block_size in np.unique(self.block_sizes, axis=0) if
-                                   0 not in block_size]
+        self.active_block_sizes = [block_size for block_size in np.unique(self.block_sizes, axis=0) if 0 not in block_size]
         self.is_identity_channels = np.array([0 in block_size for block_size in self.block_sizes])
 
-    def DReLU(self, activation):
-        return (activation >= 0).to(activation.dtype)
 
-    def mult(self, a, b):
-        return a * b
 
-    def pad(self, x, pad):
-        pad_x = pad[0] - x.shape[2] % pad[0]
-        pad_y = pad[1] - x.shape[3] % pad[1]
-
-        pad_x_l = pad_x // 2
-        pad_x_r = pad_x - pad_x_l
-        pad_y_l = pad_y // 2
-        pad_y_r = pad_y - pad_y_l
-
-        if pad_x_l > 0 or pad_x_r > 0 or pad_y_l > 0 or pad_y_r > 0:
-            x = F.pad(x, pad=(pad_y_l, pad_y_r, pad_x_l, pad_x_r), mode='constant', value=0)
-        return x
+    # def mult(self, a, b):
+    #     return a * b
 
     def forward(self, activation):
-        if type(activation) == torch.Tensor:
-            activation = activation.clone()
-        else:
-            activation = activation.copy()
 
         reshaped_inputs = []
         mean_tensors = []
         channels = []
         orig_shapes = []
-        padders = []
+        pad_handlers = []
 
         for block_size in self.active_block_sizes:
             cur_channels = [bool(x) for x in np.all(self.block_sizes == block_size, axis=1)]
             cur_input = activation[:, cur_channels]
-            padder = Pad(cur_input, block_size)
+            padder = self.pad_handler_class(cur_input, block_size)
             cur_input = padder.pad(cur_input)
-            reshaped_input = SpaceToDepth(block_size)(cur_input)
-            mean_tensor = torch.sum(reshaped_input, dim=-1, keepdim=True)
+            reshaped_input = self.space_to_depth_class(block_size)(cur_input)
+            mean_tensor = self.sum(reshaped_input)
 
             channels.append(cur_channels)
             reshaped_inputs.append(reshaped_input)
             orig_shapes.append(mean_tensor.shape)
             mean_tensors.append(mean_tensor.flatten())
-            padders.append(padder)
+            pad_handlers.append(padder)
 
         cumsum_shapes = [0] + list(np.cumsum([mean_tensor.shape[0] for mean_tensor in mean_tensors]))
-        mean_tensors = torch.cat(mean_tensors)
+        mean_tensors = self.concat(mean_tensors)
 
         sign_tensors = self.DReLU(mean_tensors)
 
-        relu_map = torch.ones_like(activation)
+        relu_map = self.ones_like(activation)  # TODO: here
         for i in range(len(self.active_block_sizes)):
             sign_tensor = sign_tensors[int(cumsum_shapes[i]):int(cumsum_shapes[i + 1])].reshape(orig_shapes[i])
-            tensor = sign_tensor.repeat((1, 1, 1, 1, reshaped_inputs[i].shape[-1]))
-            relu_map[:, channels[i]] = padders[i].unpad(DepthToSpace(self.active_block_sizes[i])(tensor))
+            repeats = reshaped_inputs[i].shape[-1]
+            tensor = self.repeat(sign_tensor, repeats)
 
-        activation[:, ~self.is_identity_channels] = self.mult(relu_map[:, ~self.is_identity_channels],
-                                                              activation[:, ~self.is_identity_channels])
+            relu_map[:, channels[i]] = pad_handlers[i].unpad(self.depth_to_space_class(self.active_block_sizes[i])(tensor))
+
+        activation[:, ~self.is_identity_channels] = self.mult(relu_map[:, ~self.is_identity_channels], activation[:, ~self.is_identity_channels])
         return activation
 
 
+class NumpySecureOptimizedBlockReLU(SecureOptimizedBlockReLU):
+
+    def __init__(self, block_sizes):
+        super().__init__(block_sizes)
+        self.concat = np.concatenate
+        self.sum = lambda tensor: np.sum(tensor, axis=-1, keepdims=True)
+        self.ones_like = np.ones_like
+        self.repeat = lambda tensor, repeats: tensor.repeat(repeats, axis=-1)
+        self.depth_to_space_class = DepthToSpaceNumpy
+        self.space_to_depth_class = SpaceToDepthNumpy
+        self.pad_handler_class = NumpyPadHandler
+
+    def DReLU(self, activation):
+        return activation >= 0
+        # return activation.sign().add(1).div(2).to(activation.dtype)
+class TorchSecureOptimizedBlockReLU(SecureOptimizedBlockReLU):
+    def __init__(self, block_sizes):
+        super().__init__(block_sizes)
+        self.concat = torch.cat
+        self.sum = lambda tensor: torch.sum(tensor, dim=-1, keepdim=True)
+        self.ones_like = torch.ones_like
+        self.repeat = lambda tensor, repeats: tensor.repeat((1, 1, 1, 1, repeats))
+        self.depth_to_space_class = DepthToSpaceTorch
+        self.space_to_depth_class = SpaceToDepthTorch
+        self.pad_handler_class = TorchPadHandler
+
+    def DReLU(self, activation):
+        return (activation >= 0).to(activation.dtype)
+        # return activation.sign().add(1).div(2).to(activation.dtype)
+
+
 if __name__ == "__main__":
-    image = torch.from_numpy(np.random.rand(1, 4, 224, 224))
     import time
 
-    relu_0 = BlockRelu([[1, 1], [2, 2], [2, 4], [3, 3]])
-    relu_1 = BlockReLU_V1([[1, 1], [2, 2], [2, 4], [3, 3]])
+    block_sizes = [[3, 4], [1, 1], [0,1], [0, 1], [0,1], [2, 2], [7, 8], [3, 3], [3, 4], [2, 4], [1, 1], [7,8], [7, 8], [11,3], [2, 4], [3, 3],  [3, 3],  [3, 3], [1, 1], [1, 1], [3, 4],[3, 3],[7, 8], [3, 3], [5, 7], [7, 8], [7, 8], [7, 8]]
+    image = np.random.normal(size=(1, len(block_sizes), 224, 224))
+    image_torch = torch.from_numpy(image)
+    relu_0 = BlockRelu(block_sizes)
+    relu_numpy = NumpySecureOptimizedBlockReLU(block_sizes)
+    relu_torch = TorchSecureOptimizedBlockReLU(block_sizes)
 
     t0 = time.time()
-    out_0 = relu_0(image)
+    out_0 = relu_0(image_torch)
     t1 = time.time()
-    out_1 = relu_1(image)
+    out_numpy = relu_numpy(image)
     t2 = time.time()
-    print(torch.all(torch.isclose(out_0, out_1)))
-    print(t2 - t1, t1 - t0)
+    out_torch = relu_torch(image_torch)
+    t3 = time.time()
+
+    # out_numpy = torch.from_numpy(out_numpy)
+    print(torch.all(torch.isclose(out_0, out_torch)))
+    print((t3 - t2) / (t2 - t1))
     print('fds')
