3307fad1056874e72c7123ea87bb7704e1b5ba49
Yakir Gorski <yakirg320@gmail.com>
2022-12-30 09:14:48 +0200

    Some more support for detection

--- research/utils.py
+++ research/utils.py
@@ -1,6 +1,6 @@
 from mmcls.datasets import build_dataset as build_dataset_mmcls
 from mmseg.datasets import build_dataset as build_dataset_mmseg
-
+from mmdet.datasets import build_dataset as build_dataset_mmdet
 def build_data(cfg, train):
 
     data_cfg = cfg.data.train if train else cfg.data.test
@@ -8,6 +8,8 @@ def build_data(cfg, train):
         dataset = build_dataset_mmcls(data_cfg, default_args=dict(test_mode=False))
     elif cfg.model.type == 'EncoderDecoder':
         dataset = build_dataset_mmseg(data_cfg, default_args=dict(test_mode=False))
+    elif cfg.model.type == 'SingleStageDetector':
+        dataset = build_dataset_mmdet(data_cfg, default_args=dict(test_mode=False))
     else:
         raise NotImplementedError
 
