5c76043c10e46fe525fa8ca8815ec51f5b31fe52
Yakir Gorski <yakirg320@gmail.com>
2023-01-01 07:10:35 +0200

    A new resnet extesion

---
+++ research/mmlab_extension/classification/resnet.py
@@ -0,0 +1,116 @@
+import torch.nn as nn
+import torch.utils.checkpoint as cp
+
+from mmcls.models.builder import BACKBONES
+from mmcls.models.backbones import ResNet
+from mmcls.models.backbones.resnet import Bottleneck, BasicBlock
+
+
+class MyBottleneck(Bottleneck):
+
+    def __init__(self,
+                 **kwargs):
+        super(MyBottleneck, self).__init__(**kwargs)
+
+        self.relu_1 = nn.ReLU(inplace=True)
+        self.relu_2 = nn.ReLU(inplace=True)
+        self.relu_3 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+
+        def _inner_forward(x):
+            identity = x
+
+            out = self.conv1(x)
+            out = self.norm1(out)
+            out = self.relu_1(out)
+
+            out = self.conv2(out)
+            out = self.norm2(out)
+            out = self.relu_2(out)
+
+            out = self.conv3(out)
+            out = self.norm3(out)
+
+            if self.downsample is not None:
+                identity = self.downsample(x)
+
+            out = self.drop_path(out)
+
+            out += identity
+
+            return out
+
+        if self.with_cp and x.requires_grad:
+            out = cp.checkpoint(_inner_forward, x)
+        else:
+            out = _inner_forward(x)
+
+        out = self.relu_3(out)
+
+        return out
+
+class MyBasicBlock(BasicBlock):
+
+    def __init__(self,
+                 **kwargs):
+        super(MyBasicBlock, self).__init__(**kwargs)
+
+        self.relu_1 = nn.ReLU(inplace=True)
+        self.relu_2 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+
+        def _inner_forward(x):
+            identity = x
+
+            out = self.conv1(x)
+            out = self.norm1(out)
+            out = self.relu_1(out)
+
+            out = self.conv2(out)
+            out = self.norm2(out)
+
+            if self.downsample is not None:
+                identity = self.downsample(x)
+
+            out = self.drop_path(out)
+
+            out += identity
+
+            return out
+
+        if self.with_cp and x.requires_grad:
+            out = cp.checkpoint(_inner_forward, x)
+        else:
+            out = _inner_forward(x)
+
+        out = self.relu_2(out)
+
+        return out
+
+
+@BACKBONES.register_module()
+class MyResNet(ResNet):
+
+    arch_settings = {
+        18: (MyBasicBlock, (2, 2, 2, 2)),
+        34: (MyBasicBlock, (3, 4, 6, 3)),
+        50: (MyBasicBlock, (3, 4, 6, 3)),
+        101: (MyBottleneck, (3, 4, 23, 3)),
+        152: (MyBottleneck, (3, 8, 36, 3))
+    }
+
+    def __init__(self, **kwargs):
+        super(MyResNet, self).__init__(**kwargs)
+
+
+@BACKBONES.register_module()
+class AvgPoolResNet(MyResNet):
+
+    def __init__(self, **kwargs):
+        super(AvgPoolResNet, self).__init__(**kwargs)
+
+    def _make_stem_layer(self, *args, **kwargs):
+        ResNet._make_stem_layer(self, *args, **kwargs)
+        self.maxpool = nn.AvgPool2d(kernel_size=3, stride=2, padding=1)
