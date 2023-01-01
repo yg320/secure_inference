d93049e6346a53561a01f87a6b3292854f36b951
Yakir Gorski <yakirg320@gmail.com>
2023-01-01 07:19:35 +0200

    Added some renset configs

---
+++ research/configs/classification/resnet/resnet18_8xb32_in1k_brelu_0.05_v3.py
@@ -0,0 +1,8 @@
+_base_ = [
+    '../_base_/models/resnet18_avg_pool.py', '../_base_/datasets/imagenet_bs32.py',
+    '../_base_/schedules/imagenet_bs256_finetune_v3.py', '../_base_/default_runtime.py'
+]
+
+relu_spec_file = "./relu_spec_files/classification/resnet18_8xb32_in1k/brelu_0.05.pickle"
+load_from = "./mmlab_models/classification/resnet18_8xb32_in1k_20210831-fbbb1da6.pth"
+work_dir = "./outputs/classification/resnet18_8xb32_in1k/brelu_0.05_v3"
