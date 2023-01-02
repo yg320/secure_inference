4e7148a900fc77f92314193015b0b057dfae5752
Yakir Gorski <yakirg320@gmail.com>
2023-01-02 19:24:42 +0200

    scratches

--- exe_script.py
+++ exe_script.py
@@ -2,17 +2,15 @@ import os
 import time
 
 os.chdir("/storage/yakir/secure_inference")
-
+INSTALL_MMDET = "pip install mmdet "
 PYTHON_PATH_EXPORT = 'export PYTHONPATH=\"${PYTHONPATH}:/storage/yakir/secure_inference\"; '
 
 # Training - dist - segmentation
-os.system(PYTHON_PATH_EXPORT +
-          'bash ./research/pipeline/dist_train.sh ./research/configs/segmentation/deeplabv3_m-v2-d8_512x512_160k_ade20k/relu_0.05.py 2')
+# os.system(PYT/esearch/pipeline/dist_train.sh ./research/configs/segmentation/deeplabv3_m-v2-d8_512x512_160k_ade20k/relu_0.05.py 2')
 
 # Training - dist - classification
-# os.system(PYTHON_PATH_EXPORT +
-#           'bash ./research/mmlab_tools/dist_train_cls.sh research/configs/classification/relu_spec_0.1/resnet18_8xb16_cifar10.py '
-#           '--load-from ./mmlab_models/classification/resnet18_b16x8_cifar10_20210528-bd6371c8.pth')
+os.system(PYTHON_PATH_EXPORT +
+          'bash ./research/mmlab_tools/classification/dist_train_cls.sh research/configs/classification/resnet/resnet18_8xb32_in1k_brelu_0.05.py 2 ')
 
 # Training - local - classification
 # os.system(PYTHON_PATH_EXPORT +
@@ -21,12 +19,12 @@ os.system(PYTHON_PATH_EXPORT +
 # Distortion extraction
 # os.system(PYTHON_PATH_EXPORT +
 #           'python research/distortion/channel_order_distortion.py '
-#           f'--batch_index 2 '
+#           f'--batch_index 0 '
 #           f'--gpu_id 0 '
-#           '--config research/configs/segmentation/baseline/deeplabv3_m-v2-d8_512x512_160k_ade20k.py '
-#           '--checkpoint mmlab_models/segmentation/deeplabv3_m-v2-d8_512x512_160k_ade20k_20200825_223255-63986343.pth '
-#           '--output_path outputs/distortions/segmentation/baseline/deeplabv3_m-v2-d8_512x512_160k_ade20k/ '
-#           '--batch_size 16')
+#           '--config research/configs/classification/resnet/resnet18_8xb32_in1k.py '
+#           '--checkpoint mmlab_models/classification/resnet18_8xb32_in1k_20210831-fbbb1da6.pth '
+#           '--output_path outputs/distortions/classification/resnet18_8xb32_in1k/ '
+#           '--batch_size 64')
 
 # KnapSack
 # os.system(PYTHON_PATH_EXPORT +
