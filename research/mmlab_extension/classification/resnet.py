c8d029303703c6daf9fa783dab1b6b2a00dc9e84
Yakir Gorski <yakirg320@gmail.com>
2023-01-02 19:25:48 +0200

    Minor bugfix

--- research/mmlab_extension/classification/resnet.py
+++ research/mmlab_extension/classification/resnet.py
@@ -9,8 +9,9 @@ from mmcls.models.backbones.resnet import Bottleneck, BasicBlock
 class MyBottleneck(Bottleneck):
 
     def __init__(self,
+                 *args,
                  **kwargs):
-        super(MyBottleneck, self).__init__(**kwargs)
+        super(MyBottleneck, self).__init__(*args, **kwargs)
 
         self.relu_1 = nn.ReLU(inplace=True)
         self.relu_2 = nn.ReLU(inplace=True)
@@ -53,8 +54,9 @@ class MyBottleneck(Bottleneck):
 class MyBasicBlock(BasicBlock):
 
     def __init__(self,
+                 *args,
                  **kwargs):
-        super(MyBasicBlock, self).__init__(**kwargs)
+        super(MyBasicBlock, self).__init__(*args, **kwargs)
 
         self.relu_1 = nn.ReLU(inplace=True)
         self.relu_2 = nn.ReLU(inplace=True)
@@ -96,21 +98,35 @@ class MyResNet(ResNet):
     arch_settings = {
         18: (MyBasicBlock, (2, 2, 2, 2)),
         34: (MyBasicBlock, (3, 4, 6, 3)),
-        50: (MyBasicBlock, (3, 4, 6, 3)),
+        50: (MyBottleneck, (3, 4, 6, 3)),
         101: (MyBottleneck, (3, 4, 23, 3)),
         152: (MyBottleneck, (3, 8, 36, 3))
     }
 
-    def __init__(self, **kwargs):
-        super(MyResNet, self).__init__(**kwargs)
+    def __init__(self, *args, **kwargs):
+        super(MyResNet, self).__init__(*args, **kwargs)
 
-
-@BACKBONES.register_module()
-class AvgPoolResNet(MyResNet):
-
-    def __init__(self, **kwargs):
-        super(AvgPoolResNet, self).__init__(**kwargs)
-
-    def _make_stem_layer(self, *args, **kwargs):
-        ResNet._make_stem_layer(self, *args, **kwargs)
-        self.maxpool = nn.AvgPool2d(kernel_size=3, stride=2, padding=1)
+# import torch
+#
+# class MyAvgPool(nn.Module):
+#
+#     def __init__(self):
+#         super(MyAvgPool, self).__init__()
+#
+#     def forward(self, x):
+#         x = torch.from_numpy(x)
+#         x = nn.AvgPool2d(kernel_size=3, stride=2, padding=1)(x)
+#         x = x.numpy()
+#         return x
+#
+#
+# @BACKBONES.register_module()
+# class AvgPoolResNet(MyResNet):
+#
+#     def __init__(self, *args, **kwargs):
+#         super(AvgPoolResNet, self).__init__(*args, **kwargs)
+#
+#     def _make_stem_layer(self, *args, **kwargs):
+#         ResNet._make_stem_layer(self, *args, **kwargs)
+#         # self.maxpool = nn.AvgPool2d(kernel_size=3, stride=2, padding=1)
+#         self.maxpool = MyAvgPool()
