0fa5e2531faeecafa369efd4995dc0fc276c5a44
Yakir Gorski <yakirg320@gmail.com>
2023-01-03 04:49:02 +0200

    new configuration files

---
+++ research/configs/classification/resnet/resnet50_8xb32_in1k_finetune.py
@@ -0,0 +1,8 @@
+_base_ = [
+    '../_base_/models/resnet50.py', '../_base_/datasets/imagenet_bs32.py',
+    '../_base_/schedules/imagenet_bs256_finetune.py', '../_base_/default_runtime.py'
+]
+
+relu_spec_file = None
+load_from = "./mmlab_models/classification/resnet50_8xb32_in1k_20210831-ea4938fc.pth"
+work_dir = "./outputs/classification/resnet50_8xb32_in1k/finetune"
