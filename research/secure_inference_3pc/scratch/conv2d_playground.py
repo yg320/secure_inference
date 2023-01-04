f2f1cad7172fbf1e03470672bb1176ee41c020c7
Yakir Gorski <yakirg320@gmail.com>
2023-01-04 08:37:21 +0200

    Setting up env for optimizing ops

--- research/secure_inference_3pc/scratch/conv2d_playground.py
+++ research/secure_inference_3pc/scratch/conv2d_playground.py
@@ -7,12 +7,12 @@ conv2d_handler = Conv2DHandler("cuda:0")
 dtype = np.int64
 low = np.iinfo(dtype).min
 high = np.iinfo(dtype).max
-repetitions = 1
+repetitions = 10
 np.random.seed(1)
 x = [np.random.randint(low, high, (1, 256, 14, 14), dtype=dtype) for _ in range(repetitions)]
 w = [np.random.randint(low, high, (256, 256, 3, 3), dtype=dtype) for _ in range(repetitions)]
 
 t0 = time.time()
 for i in range(repetitions):
-    print(conv2d_handler.conv2d(x[i], w[i], (1, 1), (1, 1), (1, 1), 1).mean())
+    conv2d_handler.conv2d(x[i], w[i], (1, 1), (1, 1), (1, 1), 1)
 print(time.time() - t0)
--- research/secure_inference_3pc/scratch/optimizing_ops.py
+++ research/secure_inference_3pc/scratch/optimizing_ops.py
@@ -1,7 +1,58 @@
-# import numpy as np
-# import time
-# import torch
-# from research.secure_inference_3pc.timer import Timer
+import numpy as np
+import time
+import torch
+from research.secure_inference_3pc.timer import Timer
+
+
+
+IGNORE_MSB_BITS = 0
+NUM_BITS = 64
+NUM_OF_COMPARE_BITS = 64
+UNSIGNED_DTYPE = np.uint64
+
+powers = np.arange(NUM_BITS, dtype=UNSIGNED_DTYPE)[np.newaxis][:,::-1]
+
+def decompose(value, out=None, out_mask=None):
+    orig_shape = list(value.shape)
+    value = value.reshape(-1, 1)
+    end = None if IGNORE_MSB_BITS == 0 else -IGNORE_MSB_BITS
+    r_shift = value >> powers[:,NUM_BITS - NUM_OF_COMPARE_BITS-IGNORE_MSB_BITS:end]
+    value_bits = np.zeros(shape=(value.shape[0], NUM_OF_COMPARE_BITS), dtype=np.int8)
+    np.bitwise_and(r_shift, np.int8(1), out=value_bits)
+    ret = value_bits.reshape(orig_shape + [NUM_OF_COMPARE_BITS])
+    return ret
+
+def get_c_party_0(x_bits, multiplexer_bits, beta, j):
+    beta = beta[..., np.newaxis]
+    beta = 2 * beta  # Not allowed to change beta inplace
+    np.subtract(beta, 1, out=beta)
+    np.multiply(multiplexer_bits, x_bits, out=multiplexer_bits)
+    np.multiply(multiplexer_bits, -2, out=multiplexer_bits)
+    np.add(multiplexer_bits, x_bits, out=multiplexer_bits)
+
+    w_cumsum = multiplexer_bits.astype(np.int32)
+    np.cumsum(w_cumsum, axis=-1, out=w_cumsum)
+    np.subtract(w_cumsum, multiplexer_bits, out=w_cumsum)
+    np.multiply(x_bits, beta, out=x_bits)
+    np.add(w_cumsum, x_bits, out=w_cumsum)
+
+    return w_cumsum
+
+N = 10000
+x_bits_0 = np.random.randint(low=0, high=67, shape=(N, NUM_BITS), dtype=np.int8)
+r = np.random.randint(low=np.iinfo(np.uint64).min, high=np.iinfo(np.uint64).max, size=(N,), dtype=np.uint64)
+beta = np.random.randint(low=0, high=2, size=(N,), dtype=np.int8)
+
+
+bits = decompose(r)
+c_bits_0 = get_c_party_0(x_bits_0, bits, beta, np.int8(0))
+
+
+
+
+
+
+
 # NUM_BITS = 64
 # UNSIGNED_DTYPE = np.uint64
 # NUM_OF_COMPARE_BITS = 64
@@ -38,10 +89,3 @@
 #
 # with Timer("decompose - numpy"):
 #     decompose(value)
-
-
-bits = decompose(r)
-c_bits_0 = get_c_party_0(x_bits_0, bits, beta, np.int8(0))
-np.multiply(s, c_bits_0, out=s)
-with Timer("module_67(s)"):
-    d_bits_0 = module_67(s)
