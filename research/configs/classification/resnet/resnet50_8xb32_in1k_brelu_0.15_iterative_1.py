b6aedb934907f5a6ed127aceeae894736c61623a
Yakir Gorski <yakirg320@gmail.com>
2023-01-11 18:50:41 +0200

    configs...

---
+++ research/configs/classification/resnet/resnet50_8xb32_in1k_brelu_0.15_iterative_1.py
@@ -0,0 +1,8 @@
+_base_ = [
+    '../_base_/models/resnet50_avg_pool.py', '../_base_/datasets/imagenet_bs32.py',
+    '../_base_/schedules/imagenet_bs256_finetune.py', '../_base_/default_runtime.py'
+]
+
+relu_spec_file = "./relu_spec_files/classification/resnet50_8xb32_in1k_iterative/iter_1/block_size_spec_0.1.pickle"
+load_from = "./outputs/classification/resnet50_8xb32_in1k/finetune_0.0001_avg_pool/epoch_13.pth"
+work_dir = "./outputs/classification/resnet50_8xb32_in1k/brelu_0.15_iterative/iter_1"
