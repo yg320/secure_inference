9e0e6bed4420b4dda592ee80b64a382aba768c1c
Yakir Gorski <yakirg320@gmail.com>
2023-01-18 14:27:12 +0200

    Configs...

--- research/configs/classification/_base_/schedules/imagenet_bs256_finetune_0.0001.py
+++ research/configs/classification/_base_/schedules/imagenet_bs256_finetune_0.0001.py
@@ -2,5 +2,5 @@
 optimizer = dict(type='SGD', lr=0.0001, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
-lr_config = dict(policy='step', step=[70, 90])
-runner = dict(type='EpochBasedRunner', max_epochs=100)
+lr_config = dict(policy='step', step=[18, 30], min_lr=0.0001, gamma=0.2)
+runner = dict(type='EpochBasedRunner', max_epochs=35)
--- research/configs/classification/_base_/schedules/imagenet_bs256_finetune_0.005.py
+++ research/configs/classification/_base_/schedules/imagenet_bs256_finetune_0.005.py
@@ -1,5 +1,5 @@
 # optimizer
-optimizer = dict(type='SGD', lr=0.0005, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(policy='step', step=[18, 30], min_lr=0.0001, gamma=0.2)
