087b5e150e8a89ed533b899893e6ff95e64c00fb
Yakir Gorski <yakirg320@gmail.com>
2023-01-26 18:28:21 +0200

    some new configs

--- research/distortion/collect_distortion.py
+++ research/distortion/collect_distortion.py
@@ -3,30 +3,77 @@ import os
 import glob
 import numpy as np
 
-input_path = ""
-out_path = ""
-layer_names = ["layer1_1_1"]
+input_path = "/storage/yakir/secure_inference/outputs_v2/distortions/classification/resnet50_8xb32_in1k/train_mode"
+out_path = "/storage/yakir/secure_inference/outputs_v2/distortions/classification/resnet50_8xb32_in1k/train_mode_collected"
+os.makedirs(os.path.join(out_path, "loss"))
+os.makedirs(os.path.join(out_path, "distortion"))
+layer_names = [
+                "stem",
+                'layer1_0_1',
+                'layer1_0_2',
+                'layer1_0_3',
+                'layer1_1_1',
+                'layer1_1_2',
+                'layer1_1_3',
+                'layer1_2_1',
+                'layer1_2_2',
+                'layer1_2_3',
+                'layer2_0_1',
+                'layer2_0_2',
+                'layer2_0_3',
+                'layer2_1_1',
+                'layer2_1_2',
+                'layer2_1_3',
+                'layer2_2_1',
+                'layer2_2_2',
+                'layer2_2_3',
+                'layer2_3_1',
+                'layer2_3_2',
+                'layer2_3_3',
+                'layer3_0_1',
+                'layer3_0_2',
+                'layer3_0_3',
+                'layer3_1_1',
+                'layer3_1_2',
+                'layer3_1_3',
+                'layer3_2_1',
+                'layer3_2_2',
+                'layer3_2_3',
+                'layer3_3_1',
+                'layer3_3_2',
+                'layer3_3_3',
+                'layer3_4_1',
+                'layer3_4_2',
+                'layer3_4_3',
+                'layer3_5_1',
+                'layer3_5_2',
+                'layer3_5_3',
+                'layer4_0_1',
+                'layer4_0_2',
+                'layer4_0_3',
+                'layer4_1_1',
+                'layer4_1_2',
+                'layer4_1_3',
+                'layer4_2_1',
+                'layer4_2_2',
+                'layer4_2_3',
+
+            ]
 
 for layer_name in layer_names:
     glob_pattern = os.path.join(input_path, f"{layer_name}_*.pickle")
     files = glob.glob(glob_pattern)
     assert len(files) == 8, glob_pattern
 
-    noise = np.stack([pickle.load(open(f, 'rb'))["Noise"] for f in files])
-    noise = noise.mean(axis=0).T
-
-    # signal = np.stack([pickle.load(open(f, 'rb'))["Signal"] for f in files])
-    # signal = signal.mean(axis=0).T
+    distortion = np.stack([pickle.load(open(f, 'rb'))["Noise"] for f in files])
+    distortion = distortion.mean(axis=0).T
+    distortion = distortion[:-1]
+    distortion = np.array(distortion).T
 
     loss = np.stack([pickle.load(open(f, 'rb'))["Distorted Loss"] for f in files])
     loss = loss.mean(axis=0).T
-
-    distortion = noise
-    distortion = distortion[:-1]
-    tt = -np.array(distortion).T
-
     loss = loss[:-1].T
 
-    np.save(os.path.join(out_path, "loss", f"{layer_name}.npy"), tt)
-    np.save(os.path.join(out_path, "distortion", f"{layer_name}.npy"), loss)
+    np.save(os.path.join(out_path, "loss", f"{layer_name}.npy"), -loss)
+    np.save(os.path.join(out_path, "distortion", f"{layer_name}.npy"), -distortion)
 
