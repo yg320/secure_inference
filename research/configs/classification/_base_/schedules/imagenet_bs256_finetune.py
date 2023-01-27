9104d2734f230bd2d0f4d49eb7133663b872034d
Yakir Gorski <yakirg320@gmail.com>
2023-01-27 05:48:49 +0200

    Configs...

--- research/configs/classification/_base_/schedules/imagenet_bs256_finetune.py
+++ research/configs/classification/_base_/schedules/imagenet_bs256_finetune.py
@@ -2,5 +2,5 @@
 optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
-lr_config = dict(policy='step', step=[70, 90])
-runner = dict(type='EpochBasedRunner', max_epochs=100)
+lr_config = dict(policy='step', step=[40, 45], warmup='linear', warmup_ratio=0.01, warmup_iters=1, warmup_by_epoch=True)
+runner = dict(type='EpochBasedRunner', max_epochs=50)
--- research/configs/classification/resnet/resnet50_8xb32_in1k_finetune_0.0001_avg_pool.py
+++
@@ -1,8 +0,0 @@
-_base_ = [
-    '../_base_/models/resnet50_avg_pool.py', '../_base_/datasets/imagenet_bs32.py',
-    '../_base_/schedules/imagenet_bs256_finetune_0.0001.py', '../_base_/default_runtime.py'
-]
-
-relu_spec_file = None
-load_from = "./mmlab_models/classification/resnet50_8xb32_in1k_20210831-ea4938fc.pth"
-work_dir = "./outputs/classification/resnet50_8xb32_in1k/finetune_0.0001_avg_pool"
