bc880f86bc484612a6050ef22c53917e45124ae8
Yakir Gorski <yakirg320@gmail.com>
2023-02-02 14:06:46 +0200

    minor change

--- research/secure_inference_3pc/params.py
+++ research/secure_inference_3pc/params.py
@@ -10,8 +10,8 @@ class Params:
     # SECURE_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "../configs/classification/resnet/resnet50_8xb32_in1k.py")
     SECURE_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "../configs/segmentation/mobilenet_v2/deeplabv3_m-v2-d8_512x512_160k_ade20k.py")
     # MODEL_PATH = "/home/yakir/PycharmProjects/secure_inference/mmlab_models/segmentation/deeplabv3_m-v2-d8_512x512_160k_ade20k_20200825_223255-63986343.pth" #None #"/home/yakir/epoch_50.pth"
-    MODEL_PATH = "/home/yakir/iter_128000.pth" #None #"/home/yakir/epoch_50.pth"
-    RELU_SPEC_FILE = "/home/yakir/block_size_spec_0.05.pickle"
+    MODEL_PATH = None #"/home/yakir/iter_128000.pth" #None #"/home/yakir/epoch_50.pth"
+    RELU_SPEC_FILE = "/home/ubuntu/block_size_spec_0.05.pickle"
     PUBLIC_IP_SERVER = ""
     PUBLIC_IP_CLIENT = ""
     PUBLIC_IP_CRYPTO_PROVIDER = ""
