56f4450ae6f52ad6b7af316e36104c11398ee5fe
Yakir Gorski <yakirg320@gmail.com>
2023-02-03 09:44:34 +0200

    AWS measurements and benchmark!

--- research/secure_inference_3pc/params.py
+++ research/secure_inference_3pc/params.py
@@ -7,21 +7,21 @@ else:
     DEVICE = "cpu"
 
 class Params:
-    SECURE_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "../configs/segmentation/mobilenet_v2/deeplabv3_m-v2-d8_512x512_160k_ade20k.py")
-    MODEL_PATH = None #"/home/yakir/PycharmProjects/secure_inference/mmlab_models/segmentation/deeplabv3_m-v2-d8_512x512_160k_ade20k_20200825_223255-63986343.pth" #None #"/home/yakir/epoch_50.pth"
-    RELU_SPEC_FILE = "/home/ubuntu/specs/seg_specs/6x6.pickle"
+    # SECURE_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "../configs/segmentation/mobilenet_v2/deeplabv3_m-v2-d8_512x512_160k_ade20k.py")
+    # MODEL_PATH = None #"/home/yakir/PycharmProjects/secure_inference/mmlab_models/segmentation/deeplabv3_m-v2-d8_512x512_160k_ade20k_20200825_223255-63986343.pth" #None #"/home/yakir/epoch_50.pth"
+    # RELU_SPEC_FILE = "/home/ubuntu/specs/seg_specs/6x6.pickle"
 
     # SECURE_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "../configs/classification/resnet/resnet50_8xb32_in1k.py")
-    # MODEL_PATH = "/home/yakir/epoch_50.pth"
+    # MODEL_PATH = None #"/home/yakir/epoch_50.pth"
     # RELU_SPEC_FILE = "/home/yakir/4x4.pickle"
 
     # SECURE_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "../configs/classification/resnet/resnet50_8xb32_in1k.py")
     # MODEL_PATH = "/home/yakir/epoch_14_avg_pool.pth"
     # RELU_SPEC_FILE = None
 
-    # SECURE_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "../configs/classification/resnet/resnet50_8xb32_in1k_maxpool.py")
-    # MODEL_PATH = "/home/yakir/PycharmProjects/secure_inference/mmlab_models/classification/resnet50_8xb32_in1k_20210831-ea4938fc.pth"
-    # RELU_SPEC_FILE = None
+    SECURE_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "../configs/classification/resnet/resnet50_8xb32_in1k_maxpool.py")
+    MODEL_PATH = None #"/home/yakir/PycharmProjects/secure_inference/mmlab_models/classification/resnet50_8xb32_in1k_20210831-ea4938fc.pth"
+    RELU_SPEC_FILE = None
 
     PUBLIC_IP_SERVER = ""
     PUBLIC_IP_CLIENT = ""
