753c132273a5330b3743c09463dd52f76669f398
Yakir Gorski <yakirg320@gmail.com>
2023-02-10 11:12:38 +0200

    Minor changes

--- research/extract_block_sizes.py
+++ research/extract_block_sizes.py
@@ -1,9 +1,11 @@
 import os
 import argparse
 import subprocess
+from research.distortion.distortion_collector import DistortionCollector
 # python research/extract_block_sizes.py --config research/configs/segmentation/mobilenet_v2/deeplabv3_m-v2-d8_512x512_160k_ade20k.py --checkpoint mmlab_models/segmentation/deeplabv3_m-v2-d8_512x512_160k_ade20k_20200825_223255-63986343.pth --output_path /storage/yakir/secure_inference/benchmark/segmentation --num_samples 48 --num_gpus 4
 # research/extract_block_sizes.py --config research/configs/classification/resnet/resnet18_2xb64_cifar100_rereduce_params.py --checkpoint /home/yakir/PycharmProjects/secure_inference/work_dirs/resnet18_2xb64_cifar100/epoch_200.pth --output_path /home/yakir/distortion_200 --num_samples 512 --num_gpus 2
-# research/extract_block_sizes.py --config research/configs/classification/resnet/resnet18_2xb64_cifar100_rereduce_params.py --checkpoint /home/yakir/PycharmProjects/secure_inference/work_dirs/resnet18_2xb64_cifar100/epoch_199.pth --output_path /home/yakir/distortion_199 --num_samples 4096 --num_gpus 2
+# research/extract_block_sizes.py --config research/configs/classification/resnet/resnet18_2xb64_cifar100_rereduce_params.py --checkpoint /home/yakir/PycharmProjects/secure_inference/work_dirs/resnet18_2xb64_cifar100/epoch_199.pth --output_path /home/yakir/distortion_199 --num_samples 8192 --num_gpus 4
+# python /home/yakir/PycharmProjects/secure_inference/research/extract_block_sizes.py --config /home/yakir/PycharmProjects/secure_inference/research/configs/classification/resnet/resnet18_2xb64_cifar100_mini.py --checkpoint /home/yakir/PycharmProjects/secure_inference/work_dirs/resnet18_2xb64_cifar100_mini/epoch_200.pth --output_path /home/yakir/distortion_200_mini --num_samples 1024 --num_gpus 2
 parser = argparse.ArgumentParser(description='')
 
 parser.add_argument('--config', type=str, default="/home/yakir/PycharmProjects/secure_inference/research/configs/segmentation/mobilenet_v2/deeplabv3_m-v2-d8_512x512_160k_ade20k_relu.py")
@@ -20,7 +22,7 @@ batch_size = args.num_samples // args.num_gpus
 distortion_extraction_processes = []
 
 chdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(chdir)
+
 os.chdir(chdir)
 distortion_output_path = os.path.join(args.output_path, "distortion_row")
 PYTHON_PATH_EXPORT = 'export PYTHONPATH=\"${PYTHONPATH}:' + chdir + '"; '
@@ -43,5 +45,5 @@ for gpu_index in range(args.num_gpus):
     )
 
 exit_codes = [p.wait() for p in distortion_extraction_processes]
-#
-# DistortionCollector(output_path=distortion_output_path).collect()
+
+# DistortionCollector(base_path=distortion_output_path).collect()
