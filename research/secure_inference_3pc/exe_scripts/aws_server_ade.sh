46215d65c6b5663e94302c29a56c9d57b4cd0c84
Yakir Gorski <yakirg320@gmail.com>
2023-03-02 11:04:39 +0200

    AWS

--- research/secure_inference_3pc/exe_scripts/aws_server_ade.sh
+++ research/secure_inference_3pc/exe_scripts/aws_server_ade.sh
@@ -1,4 +1,4 @@
-RELU_SPEC_FILE=/home/ubuntu/specs/ade/0.06.pickle
+RELU_SPEC_FILE=/home/ubuntu/specs/ade/0.09.pickle
 SECURE_CONFIG_PATH=/home/ubuntu/secure_inference/research/configs/segmentation/mobilenet_v2/deeplabv3_m-v2-d8_512x512_160k_ade20k_relu_secure_aspp.py
 
 export PYTHONPATH="." ; python research/secure_inference_3pc/parties/server/run.py --secure_config_path $SECURE_CONFIG_PATH --relu_spec_file $RELU_SPEC_FILE  &
