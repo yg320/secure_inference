d3f8341c39521ed74573ba5d6f09b31290f51eac
Yakir Gorski <yakirg320@gmail.com>
2023-03-02 16:43:13 +0200

    AWS

--- research/analysis/standalone_eval_ade.py
+++ research/analysis/standalone_eval_ade.py
@@ -3,12 +3,12 @@ import numpy as np
 import glob
 
 
-def eval(results):
+def evaluate(results):
     results = tuple(zip(*results))
     total_area_intersect = sum(results[0])
     total_area_union = sum(results[1])
     iou = total_area_intersect / total_area_union
-    return iou.mean()
+    return np.nanmean(iou)
 
 files = glob.glob("/home/yakir/evaluation/0.06/ade20k_lsb_0_msb_0_t_12/*")
 results_secure = [tuple(np.load(x, allow_pickle=True)) for x in files]
@@ -16,21 +16,12 @@ sample_ids = [int(x.split("/")[-1].split(".")[0]) for x in files]
 results_non_secure = pickle.load(open("/home/yakir/results_ade.pickle", "rb"))
 results_non_secure = [results_non_secure[x] for x in sample_ids]
 
-pre_eval_results = tuple(zip(*results_non_secure))
-total_area_intersect = sum(pre_eval_results[0])
-total_area_union = sum(pre_eval_results[1])
-total_area_pred_label = sum(pre_eval_results[2])
-total_area_label = sum(pre_eval_results[3])
-iou_not_secure = total_area_intersect / total_area_union
-iou_not_secure = np.nanmean(iou_not_secure.numpy())
-
-pre_eval_results = tuple(zip(*results_secure))
-total_area_intersect = sum(pre_eval_results[0])
-total_area_union = sum(pre_eval_results[1])
-total_area_pred_label = sum(pre_eval_results[2])
-total_area_label = sum(pre_eval_results[3])
-iou_secure = total_area_intersect / total_area_union
-iou_secure = np.nanmean(iou_secure.numpy())
-print(iou_secure, iou_not_secure, iou_secure/iou_not_secure)
+x = -1
+for i in range(len(files)):
+    iou_not_secure = evaluate(results_non_secure[:i] + results_non_secure[i+1:])
+    iou_secure = evaluate(results_secure[:i] + results_secure[i+1:])
+    x = max(x, iou_secure/iou_not_secure)
+    # print(iou_secure, iou_not_secure, iou_secure/iou_not_secure)
+print(x)
 print(len(files))
 # 72
--- research/secure_inference_3pc/exe_scripts/aws_client_cifar.sh
+++ research/secure_inference_3pc/exe_scripts/aws_client_cifar.sh
@@ -1,6 +1,6 @@
-RELU_SPEC_FILE=/home/ubuntu/specs/cifar/0.12.pickle
+RELU_SPEC_FILE=/home/ubuntu/specs/cifar/0.03.pickle
 SECURE_CONFIG_PATH=/home/ubuntu/secure_inference/research/configs/classification/resnet/resnet18_cifar100/baseline.py
 
-export PYTHONPATH="." ; python research/secure_inference_3pc/parties/client/run.py --secure_config_path $SECURE_CONFIG_PATH --relu_spec_file $RELU_SPEC_FILE --dummy_image --image_start 0 --image_end 4 &
-#export PYTHONPATH="." ; python research/secure_inference_3pc/parties/client/run.py --secure_config_path $SECURE_CONFIG_PATH --dummy_image --image_start 0 --image_end 4 &
+export PYTHONPATH="." ; python research/secure_inference_3pc/parties/client/run.py --secure_config_path $SECURE_CONFIG_PATH --relu_spec_file $RELU_SPEC_FILE --dummy_image --image_start 0 --image_end 11 &
+#export PYTHONPATH="." ; python research/secure_inference_3pc/parties/client/run.py --secure_config_path $SECURE_CONFIG_PATH --dummy_image --image_start 0 --image_end 11 &
 
