750b5dad85029df1bd3a3f6a75ba41eb4263f432
Yakir Gorski <yakirg320@gmail.com>
2023-03-02 17:03:06 +0200

    AWS

--- research/secure_inference_3pc/exe_scripts/aws_client_imagenet.sh
+++ research/secure_inference_3pc/exe_scripts/aws_client_imagenet.sh
@@ -2,6 +2,6 @@ RELU_SPEC_FILE=/home/ubuntu/specs/imagenet/0.18.pickle
 SECURE_CONFIG_PATH=/home/ubuntu/secure_inference/research/configs/classification/resnet/resnet50_in1k/resnet50_in1k_avg_pool.py
 #SECURE_CONFIG_PATH=/home/ubuntu/secure_inference/research/configs/classification/resnet/resnet50_in1k/resnet50_in1k_maxpool.py
 
-export PYTHONPATH="." ; python research/secure_inference_3pc/parties/client/run.py --secure_config_path $SECURE_CONFIG_PATH --relu_spec_file $RELU_SPEC_FILE --dummy_image --image_start 0 --image_end 11 &
-#export PYTHONPATH="." ; python research/secure_inference_3pc/parties/client/run.py --secure_config_path $SECURE_CONFIG_PATH --dummy_image --image_start 0 --image_end 11 &
+#export PYTHONPATH="." ; python research/secure_inference_3pc/parties/client/run.py --secure_config_path $SECURE_CONFIG_PATH --relu_spec_file $RELU_SPEC_FILE --dummy_image --image_start 0 --image_end 11 &
+export PYTHONPATH="." ; python research/secure_inference_3pc/parties/client/run.py --secure_config_path $SECURE_CONFIG_PATH --dummy_image --image_start 0 --image_end 11 &
 
