f698647cfc2ff1d963dceb5afc0bc57ab3a3be66
Yakir Gorski <yakirg320@gmail.com>
2023-03-02 10:35:57 +0200

    AWS

--- research/secure_inference_3pc/exe_scripts/aws_crypto_imagenet.sh
+++ research/secure_inference_3pc/exe_scripts/aws_crypto_imagenet.sh
@@ -2,6 +2,6 @@ RELU_SPEC_FILE=/home/ubuntu/specs/imagenet/0.18.pickle
 SECURE_CONFIG_PATH=/home/ubuntu/secure_inference/research/configs/classification/resnet/resnet50_in1k/resnet50_in1k_avg_pool.py
 #SECURE_CONFIG_PATH=/home/ubuntu/secure_inference/research/configs/classification/resnet/resnet50_in1k/resnet50_in1k_maxpool.py
 
-export PYTHONPATH="." ; python research/secure_inference_3pc/parties/crypto_provider/run.py --secure_config_path $SECURE_CONFIG_PATH --relu_spec_file $RELU_SPEC_FILE  &
-#export PYTHONPATH="." ; python research/secure_inference_3pc/parties/crypto_provider/run.py --secure_config_path $SECURE_CONFIG_PATH  &
+#export PYTHONPATH="." ; python research/secure_inference_3pc/parties/crypto_provider/run.py --secure_config_path $SECURE_CONFIG_PATH --relu_spec_file $RELU_SPEC_FILE  &
+export PYTHONPATH="." ; python research/secure_inference_3pc/parties/crypto_provider/run.py --secure_config_path $SECURE_CONFIG_PATH  &
 
