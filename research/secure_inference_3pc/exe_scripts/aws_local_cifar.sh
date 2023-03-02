89c1bde2a2b2f2b68422ac64004d67e8362d4c92
Yakir Gorski <yakirg320@gmail.com>
2023-03-02 08:23:19 +0200

    AWS

--- research/secure_inference_3pc/exe_scripts/aws_local_cifar.sh
+++ research/secure_inference_3pc/exe_scripts/aws_local_cifar.sh
@@ -4,6 +4,6 @@ SECURE_CONFIG_PATH=/home/ubuntu/secure_inference/research/configs/classification
 
 export PYTHONPATH="." ; python research/secure_inference_3pc/parties/server/run.py --relu_spec_file $RELU_SPEC_FILE --secure_config_path $SECURE_CONFIG_PATH &
 export PYTHONPATH="." ; python research/secure_inference_3pc/parties/crypto_provider/run.py --relu_spec_file $RELU_SPEC_FILE --secure_config_path $SECURE_CONFIG_PATH &
-export PYTHONPATH="." ; python research/secure_inference_3pc/parties/client/run.py --relu_spec_file $RELU_SPEC_FILE --secure_config_path $SECURE_CONFIG_PATH --dummy_image &
+export PYTHONPATH="." ; python research/secure_inference_3pc/parties/client/run.py --relu_spec_file $RELU_SPEC_FILE --secure_config_path $SECURE_CONFIG_PATH --dummy_image --image_start 0 --image_end 3 &
 
 
