7a464e060a3c44191390ccfb645eccd29db50c1d
Yakir Gorski <yakirg320@gmail.com>
2023-03-07 09:24:35 +0200

    AWS

--- research/secure_inference_3pc/exe_scripts/aws_client_cifar.sh
+++ research/secure_inference_3pc/exe_scripts/aws_client_cifar.sh
@@ -1,4 +1,4 @@
-RELU_SPEC_FILE=/home/ubuntu/specs/cifar/0.15.pickle
+RELU_SPEC_FILE=/home/ubuntu/specs/cifar/0.12.pickle
 SECURE_CONFIG_PATH=/home/ubuntu/secure_inference/research/configs/classification/resnet/resnet18_cifar100/baseline.py
 
 export PYTHONPATH="." ; python research/secure_inference_3pc/parties/client/run.py --secure_config_path $SECURE_CONFIG_PATH --relu_spec_file $RELU_SPEC_FILE --dummy_image --image_start 0 --image_end 11 &
