f6e663d6d5f100333b65c1aaea9b391cf3ee2961
Yakir Gorski <yakirg320@gmail.com>
2023-03-07 08:33:29 +0200

    AWS

--- research/secure_inference_3pc/exe_scripts/aws_server_cifar.sh
+++ research/secure_inference_3pc/exe_scripts/aws_server_cifar.sh
@@ -1,5 +1,5 @@
-RELU_SPEC_FILE=/home/ubuntu/specs/cifar_lightweight/0.03.pickle
-SECURE_CONFIG_PATH=/home/ubuntu/secure_inference/research/configs/classification/resnet/resnet18_cifar100/lightweight.py
+RELU_SPEC_FILE=/home/ubuntu/specs/cifar/0.06.pickle
+SECURE_CONFIG_PATH=/home/ubuntu/secure_inference/research/configs/classification/resnet/resnet18_cifar100/baseline.py
 
 export PYTHONPATH="." ; python research/secure_inference_3pc/parties/server/run.py --secure_config_path $SECURE_CONFIG_PATH --relu_spec_file $RELU_SPEC_FILE  &
 #export PYTHONPATH="." ; python research/secure_inference_3pc/parties/server/run.py --secure_config_path $SECURE_CONFIG_PATH  &
