e2956e14114b2be923dd599aeeea94caa6ee5862
John Doe <knapsack.secure.inference@gmail.com>
2023-03-16 11:01:59 +0200

    Removed supp code from master

--- supplementary_code/sandbox/resnet_params/old/different_dist_methods/test_mode_loss.py
+++
@@ -1,8 +0,0 @@
-_base_ = [
-    '../../_base_/models/resnet50_avg_pool.py', '../../_base_/datasets/imagenet_bs64.py',
-    '../../_base_/schedules/imagenet_bs256_finetune_0.005_15_epochs.py', '../../_base_/default_runtime.py'
-]
-
-relu_spec_file = "./relu_spec_files_v2/classification/resnet50_8xb32_in1k/test_mode_loss_0.1.pickle"
-load_from = "./outputs/classification/resnet50_8xb32_in1k/finetune_0.0001_avg_pool/epoch_14.pth"
-work_dir = "./outputs_v2/distortions/classification/resnet50_8xb32_in1k/different_dist_methods/test_mode_loss"
--- supplementary_code/sandbox/resnet_params/old/iter01_naive4x4_0.005.py
+++
@@ -1,8 +0,0 @@
-_base_ = [
-    '../_base_/models/resnet50_avg_pool.py', '../_base_/datasets/imagenet_bs128.py',
-    '../_base_/schedules/imagenet_bs256_finetune_0.005.py', '../_base_/default_runtime.py'
-]
-
-relu_spec_file = "./relu_spec_files/classification/resnet50/block_size_spec_4x4_naive.pickle"
-load_from = "./outputs/classification/resnet50_8xb32_in1k/finetune_0.0001_avg_pool/epoch_14.pth"
-work_dir = "./outputs/classification/resnet50_8xb32_in1k/iter01_naive4x4_0.005"
